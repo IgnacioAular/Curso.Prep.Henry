e1a43a8f3623801180f7d2fccdd91ada67ca0871
ignacio_345@hotmail.com <ignacio_345@hotmail.com>
2021-11-04 09:25:40 -0500

    Homework number four have been finished

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -206,10 +206,12 @@ function breakStatement(numero) {
 
 
 function continueStatement(numero) {
-  //Iterar en un bucle aumentando en 2 el numero recibido hasta un límite de 10 veces.
+  //Iterar en un bucle aumentando en 2 el numero recibido 
+  //hasta un límite de 10 veces.
   //Guardar cada nuevo valor en un array.    
   //Devolver el array
-  //Cuando el número de iteraciones alcance el valor 5, no se suma en ese caso y se continua con la siguiente iteración
+  //Cuando el número de iteraciones alcance el valor 5, 
+  //no se suma en ese caso y se continua con la siguiente iteración
   //Pista: usá el statement 'continue'
   // Tu código:
   var arr = [];
